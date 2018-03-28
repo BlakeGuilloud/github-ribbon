39ff1f7c70fb1f9397d06bb24a471c978fc5bf7e
Blake Guilloud <blake.guilloud@gmail.com>
2018-03-28 18:55:57 -0400

    Blake - initial commit.

---
+++ src/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ribbonSrcSuffix = (color) => {
+  switch (color) {
+    case 'darkblue':
+      return 'darkblue_121621';
+    case 'green':
+      return 'green_007200';
+    case 'orange':
+      return 'orange_ff7600';
+    case 'white':
+      return 'white_ffffff';
+    case 'gray':
+    case 'grey':
+      return 'gray_6d6d6d';
+    default:
+      return 'red_aa0000';
+  }
+};
+
+const RibbonImage = styled.img.attrs({
+  src: props => props.src,
+  alt: props => props.alt,
+})`
+  position: absolute;
+  top: 0;
+  right: ${props => props.position === 'right' && 0};
+  left: ${props => props.position === 'left' && 0};
+`;
+
+const GitHubRibbon = ({ alt, link, position, color }) => {
+  const ribbonSrc = `https://s3.amazonaws.com/github/ribbons/forkme_${position}_${ribbonSrcSuffix(color)}.png`;
+
+  return (
+    <a href={link}>
+      <RibbonImage src={ribbonSrc} alt={alt} />
+    </a>
+  );
+};
+
+GitHubRibbon.propTypes = {
+  alt: PropTypes.string,
+  color: PropTypes.string,
+  link: PropTypes.string,
+  position: PropTypes.string,
+};
+
+GitHubRibbon.defaultProps = {
+  alt: 'Fork me on GitHub',
+  color: 'darkblue',
+  link: 'https://github.com',
+  position: 'left',
+};
+
+export default GitHubRibbon;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const paths = {
+  dist: path.resolve(__dirname, 'dist'),
+  src: path.resolve(__dirname, 'src'),
+};
+
+module.exports = () => ({
+  entry: path.join(paths.src, 'index.jsx'),
+  output: {
+    path: paths.dist,
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+});
